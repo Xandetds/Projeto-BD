e4b6496e6f9acca507c84360109f25c9d0e53c33
Alexandre Tibes <alexandretibes9@gmail.com>
2025-06-25 20:03:15 -0300

    fix: arruma estrutura

--- database/views/view_conteudos_mais_populares.sql
+++ database/views/view_conteudos_mais_populares.sql
@@ -1,5 +1,3 @@
--- Propósito: Lista os conteúdos mais populares da plataforma, ordenados pelo número de visualizações.
-
 CREATE OR REPLACE VIEW view_conteudos_mais_populares AS
 SELECT
     c.id_conteudo,        
@@ -16,4 +14,3 @@ JOIN
 ORDER BY
     c.visualizacoes DESC, c.titulo ASC;
 
--- Exemplo de uso: SELECT * FROM view_conteudos_mais_populares;
